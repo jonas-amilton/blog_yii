f60684c88559eb52a539a96705b7a469185c77d2
jonas-amilton <jonas96@edu.unisinos.br>
2025-02-01 20:42:38 -0300

    refactor: adicionado ao formulário de login novo texto, a action login, método post e link para cadastro de usuario

--- views/site/login.php
+++ views/site/login.php
@@ -7,6 +7,7 @@
 
 use yii\bootstrap5\ActiveForm;
 use yii\bootstrap5\Html;
+use yii\helpers\Url;
 
 $this->title = 'Login';
 $this->params['breadcrumbs'][] = $this->title;
@@ -14,13 +15,15 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="site-login">
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>Please fill out the following fields to login:</p>
+    <p>Faça login no Blog!</p>
 
     <div class="row">
         <div class="col-lg-5">
 
             <?php $form = ActiveForm::begin([
                 'id' => 'login-form',
+                'action' => ['site/login'],
+                'method' => 'post',
                 'fieldConfig' => [
                     'template' => "{label}\n{input}\n{error}",
                     'labelOptions' => ['class' => 'col-lg-1 col-form-label mr-lg-3'],
@@ -46,8 +49,8 @@ $this->params['breadcrumbs'][] = $this->title;
             <?php ActiveForm::end(); ?>
 
             <div style="color:#999;">
-                You may login with <strong>admin/admin</strong> or <strong>demo/demo</strong>.<br>
-                To modify the username/password, please check out the code <code>app\models\User::$users</code>.
+                Não tem uma conta?
+                <a href="<?= Url::to(['site/register']);?>">Cadastre-se</a>
             </div>
 
         </div>
