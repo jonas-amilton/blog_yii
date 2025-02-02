325c0361779152ff2b80f8872261aa84ecf423fd
jonas-amilton <jonas96@edu.unisinos.br>
2025-02-01 22:13:54 -0300

    feat: criado model para upload de imagem

---
+++ models/UploadForm.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace app\models;
+
+use yii\base\Model;
+use yii\web\UploadedFile;
+use Yii;
+
+class UploadForm extends Model
+{
+    /**
+     * @var UploadedFile
+     */
+    public $image_file;
+
+    public function rules()
+    {
+        return [
+            [['image_file'], 'file', 'skipOnEmpty' => false, 'extensions' => 'png, jpg'],
+        ];
+    }
+
+    public function upload()
+    {
+        if ($this->validate()) {
+
+            $alias = Yii::getAlias('@uploads');
+
+            $this->image_file->saveAs($alias . $this->image_file->baseName . '.' . $this->image_file->extension);
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
