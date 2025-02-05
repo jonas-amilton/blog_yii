a2f3a5c1ebdb3368c2184cda0ad021edae9127e1
jonas-amilton <jonas96@edu.unisinos.br>
2025-02-04 21:35:46 -0300

    refactor: alterado a página de publicação no blog, adicionado titulo e breadcrumbs, alterado a action, adicionado enctype para envio de imagem e adicionado modelo para enviar e salvar publicação no banco de dados

--- views/post/index.php
+++ views/post/index.php
@@ -2,6 +2,9 @@
 
 use yii\bootstrap5\ActiveForm;
 use yii\bootstrap5\Html;
+
+$this->title = 'Criar Post';
+$this->params['breadcrumbs'][] = $this->title;
 ?>
 
 
@@ -10,16 +13,19 @@ use yii\bootstrap5\Html;
         <div class="card-body">
             <?php $form = ActiveForm::begin([
                 'id' => 'post-form',
-                'action' => ['post/create-post'],
+                'action' => ['post/create'],
                 'method' => 'post',
+                'options' => ['enctype' => 'multipart/form-data']
             ]); ?>
 
-            <?= $form->field($model, 'title')->textInput(['autofocus' => true]) ?>
+            <?= $form->field($modelPostForm, 'title')->textInput(['autofocus' => true]) ?>
 
-            <?= $form->field($model, 'content')->textarea(['rows' => 6]) ?>
+            <?= $form->field($modelPostForm, 'content')->textarea(['rows' => 6]) ?>
+
+            <?= $form->field($modelPostForm, 'image_file')->fileInput() ?>
 
             <div class="form-group">
-                <?= Html::submitButton('Submit', ['class' => 'btn btn-primary', 'name' => 'post-button']) ?>
+                <?= Html::submitButton('Criar', ['class' => 'btn btn-primary', 'name' => 'post-button']) ?>
             </div>
 
             <?php ActiveForm::end(); ?>
