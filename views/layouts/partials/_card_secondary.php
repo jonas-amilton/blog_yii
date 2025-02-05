d60d9dfa8fcf13e3535e0993d92ed3f4dcdbf4ef
jonas-amilton <jonas96@edu.unisinos.br>
2025-02-05 20:59:39 -0300

    feat: adicionado dados do banco de dados para visualizar publicações e imagens

--- views/layouts/partials/_card_secondary.php
+++ views/layouts/partials/_card_secondary.php
@@ -1,10 +1,25 @@
-<div class="col-lg-4 col-md-6 col-sm-12 mb-2">
-    <div class="card card-border-none" style="width: 100%;">
-        <img src="<?= 'https://images.pexels.com/photos/28539583/pexels-photo-28539583/free-photo-of-majestic-mountain-peaks-at-sunrise.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1;' ?>" class="card-img-top" alt="...">
-        <div class="card-body">
-            <p class="card-text"><small class="text-body-secondary"><?= date('d/m/Y') ?></small></p>
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+<?php foreach ($posts as $post): ?>
+    <div class="col-lg-4 col-md-6 col-sm-12 mb-2">
+        <div class="card card-border-none" style="width: 100%;">
+            <?php foreach ($post->images as $image): ?>
+                <img
+                    src="<?= Yii::getAlias('@showImage') . "{$image->name}.{$image->extension}"; ?>"
+                    class="card-img-top"
+                    alt="<?= 'Imagem de ' . $post->title; ?>">
+            <?php endforeach; ?>
+            <div class="card-body">
+                <p class="text-body-secondary">
+                    <!--TODO: colocar link do perfil no username-->
+                    Publicado por <a href="#" class="text-reset"><?= $post->user->username; ?></a>.
+                </p>
+                <p class="card-text">
+                    <small class="text-body-secondary">
+                        Publicado em: <?= date('d/m/Y h:m', strtotime($post->created_at)); ?>
+                    </small>
+                </p>
+                <h5 class="card-title"><?= $post->title; ?></h5>
+                <p class="card-text"><?= $post->content; ?></p>
+            </div>
         </div>
     </div>
-</div>
+<?php endforeach; ?>
