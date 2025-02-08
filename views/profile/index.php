5277b4e3e2a0530ef727003bb22d54f5a2dcbc99
jonas-amilton <jonas96@edu.unisinos.br>
2025-02-07 23:36:13 -0300

    feat: criado view e css para página de perfil do usuário

---
+++ views/profile/index.php
@@ -0,0 +1,91 @@
+<div class="card" style="width: 100%;">
+    <div class="card-shadow">
+        <img src="https://images.pexels.com/photos/268941/pexels-photo-268941.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            class="card-img-top profile-cover"
+            alt="...">
+        <img src="https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            class="card-img-top profile-img"
+            alt="...">
+    </div>
+</div>
+
+<div class="container mt-5">
+    <div class="card-body">
+        <figure class="text-start">
+            <blockquote class="blockquote">
+                <p>Username</p>
+            </blockquote>
+            <figcaption class="blockquote-footer">
+                Someone famous in <cite title="Source Title">Bio</cite>
+            </figcaption>
+        </figure>
+    </div>
+</div>
+
+<div class="card mb-3" style="width: 100%;">
+    <div class="row g-0">
+        <div class="col-md-4">
+            <img src="https://images.pexels.com/photos/28184434/pexels-photo-28184434/free-photo-of-mar-natureza-agua-azul.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                class="img-fluid rounded-start"
+                alt="...">
+        </div>
+        <div class="col-md-8">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="card mb-3" style="width: 100%;">
+    <div class="row g-0">
+        <div class="col-md-4">
+            <img src="https://images.pexels.com/photos/29326451/pexels-photo-29326451/free-photo-of-naufragio-rustico-em-uma-praia-intocada.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                class="img-fluid rounded-start"
+                alt="...">
+        </div>
+        <div class="col-md-8">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="card mb-3" style="width: 100%;">
+    <div class="row g-0">
+        <div class="col-md-4">
+            <img src="https://images.pexels.com/photos/30221869/pexels-photo-30221869/free-photo-of-penhascos-costeiros-dramaticos-na-madeira-portugal.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                class="img-fluid rounded-start"
+                alt="...">
+        </div>
+        <div class="col-md-8">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="card mb-3" style="width: 100%;">
+    <div class="row g-0">
+        <div class="col-md-4">
+            <img src="https://images.pexels.com/photos/30481070/pexels-photo-30481070.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                class="img-fluid rounded-start"
+                alt="...">
+        </div>
+        <div class="col-md-8">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+            </div>
+        </div>
+    </div>
+</div>
