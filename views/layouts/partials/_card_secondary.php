df8bb3fae77d8c64fa2b1601c887ccd4b536e217
jonas-amilton <jonas96@edu.unisinos.br>
2025-02-07 22:46:32 -0300

    fix: adicionado validação para quando algum post estiver nulo

--- views/layouts/partials/_card_secondary.php
+++ views/layouts/partials/_card_secondary.php
@@ -1,25 +1,27 @@
 <?php foreach ($posts as $post): ?>
-    <div class="col-lg-4 col-md-6 col-sm-12 mb-2">
-        <div class="card card-border-none" style="width: 100%;">
-            <?php foreach ($post->images as $image): ?>
-                <img
-                    src="<?= Yii::getAlias('@showImage') . "{$image->name}.{$image->extension}"; ?>"
-                    class="card-img-top"
-                    alt="<?= 'Imagem de ' . $post->title; ?>">
-            <?php endforeach; ?>
-            <div class="card-body">
-                <p class="text-body-secondary">
-                    <!--TODO: colocar link do perfil no username-->
-                    Publicado por <a href="#" class="text-reset"><?= $post->user->username; ?></a>.
-                </p>
-                <p class="card-text">
-                    <small class="text-body-secondary">
-                        Publicado em: <?= date('d/m/Y h:m', strtotime($post->created_at)); ?>
-                    </small>
-                </p>
-                <h5 class="card-title"><?= $post->title; ?></h5>
-                <p class="card-text"><?= $post->content; ?></p>
+    <?php if (isset($post)): ?>
+        <div class="col-lg-4 col-md-6 col-sm-12 mb-2">
+            <div class="card card-border-none" style="width: 100%;">
+                <?php foreach ($post->images as $image): ?>
+                    <img
+                        src="<?= Yii::getAlias('@showImage') . "{$image->name}.{$image->extension}"; ?>"
+                        class="card-img-top"
+                        alt="<?= 'Imagem de ' . $post->title; ?>">
+                <?php endforeach; ?>
+                <div class="card-body">
+                    <p class="text-body-secondary">
+                        <!--TODO: colocar link do perfil no username-->
+                        Publicado por <a href="#" class="text-reset"><?= $post->user->username; ?></a>.
+                    </p>
+                    <p class="card-text">
+                        <small class="text-body-secondary">
+                            Publicado em: <?= date('d/m/Y h:m', strtotime($post->created_at)); ?>
+                        </small>
+                    </p>
+                    <h5 class="card-title"><?= $post->title; ?></h5>
+                    <p class="card-text"><?= $post->content; ?></p>
+                </div>
             </div>
         </div>
-    </div>
+    <?php endif; ?>
 <?php endforeach; ?>
