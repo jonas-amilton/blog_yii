36924ffb90ba6db83eca513bfb9e501fef2a8e75
jonas-amilton <jonas96@edu.unisinos.br>
2025-02-09 13:19:16 -0300

    feat: criado action para deletar publicação do usuário logado

--- controllers/PostController.php
+++ controllers/PostController.php
@@ -2,7 +2,10 @@
 
 namespace app\controllers;
 
+use app\models\Image;
+use app\models\Post;
 use app\models\PostForm;
+use app\models\UploadForm;
 use Yii;
 use yii\web\Controller;
 
@@ -44,5 +47,48 @@ class PostController extends Controller
 
             return $this->redirect(['/post/index']);
         }
+    }
+
+    public function actionDelete($id, $userId)
+    {
+        if (Yii::$app->user->id != $userId) {
+            Yii::$app->session->setFlash(
+                "danger",
+                "Exclusão não permitida, você não é o criador da publicação!"
+            );
+            return $this->redirect(['site/index']);
+        }
+
+        $imageToDelete = Image::findOne(['post_id' => $id]);
+
+        if ($imageToDelete) {
+            $filenameToDelete = "{$imageToDelete->name}.{$imageToDelete->extension}";
+            $imageToDelete->delete();
+            UploadForm::delete($filenameToDelete);
+        }
+
+        $postToDelete = Post::findOne($id);
+
+        if (!$postToDelete) {
+            Yii::$app->session->setFlash(
+                "warning",
+                "Falha na exclusão! Não foi possível encontrar a publicação."
+            );
+            return $this->redirect(['site/index']);
+        }
+
+        if ($postToDelete->delete()) {
+            Yii::$app->session->setFlash(
+                "success",
+                "Sua publicação foi deletada com sucesso!"
+            );
+            return $this->redirect(['site/index']);
+        }
+
+        Yii::$app->session->setFlash(
+            "danger",
+            "Erro na solicitação, tente novamente."
+        );
+        return $this->redirect(['site/index']);
     }
 }
