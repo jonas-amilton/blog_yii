49dee2d9f2ba6a24116062cc6ef14fe2b2d34ac3
jonas-amilton <jonas96@edu.unisinos.br>
2025-02-09 20:54:56 -0300

    feat: criado controller para personalizar o perfil do usuário

--- controllers/ProfileController.php
+++ controllers/ProfileController.php
@@ -2,6 +2,7 @@
 
 namespace app\controllers;
 
+use app\models\ProfileForm;
 use yii\web\Controller;
 use Yii;
 
@@ -9,6 +10,30 @@ class ProfileController extends Controller
 {
     public function actionIndex()
     {
-        return $this->render('index');
+        $modelProfileForm = new ProfileForm();
+
+        return $this->render('index', compact('modelProfileForm'));
+    }
+
+    public function actionEdit()
+    {
+        if (!Yii::$app->user->identity) {
+            Yii::$app->session->setFlash('warning', 'Você precisa estar logado para acessar essa página.');
+            return $this->redirect(['site/login']);
+        }
+        $modelProfileForm = new ProfileForm();
+
+        if (
+            Yii::$app->request->isPost
+            && $modelProfileForm->load(Yii::$app->request->post())
+            && $modelProfileForm->saveProfile()
+        ) {
+            Yii::$app->session->setFlash(
+                'success',
+                'Perfil alterado com sucesso!'
+            );
+
+            return $this->redirect(['profile/index']);
+        }
     }
 }
