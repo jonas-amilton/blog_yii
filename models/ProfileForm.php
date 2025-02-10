c00c693282be7d6a8067626ea03ebf5550598194
jonas-amilton <jonas96@edu.unisinos.br>
2025-02-09 21:05:37 -0300

    feat: criado model para processar formulário de personalização de usuário

---
+++ models/ProfileForm.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+use yii\base\Model;
+use yii\web\UploadedFile;
+
+class ProfileForm extends Model
+{
+    public $age;
+    public $gender;
+    public $bio;
+    public $image_file;
+    public $user_id;
+
+    public function __construct()
+    {
+        $this->user_id = Yii::$app->user->identity->id ?? null;
+    }
+
+    /**
+     * @return array the validation rules.
+     */
+    public function rules()
+    {
+        return [
+            [['bio', 'age', 'image_file', 'user_id', 'gender'], 'required'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'user_id' => 'ID do Usuário',
+            'avatar_id' => 'Avatar ID',
+            'bio' => 'Bio',
+            'age' => 'Idade',
+            'gender' => 'Sexo',
+            'image_file' => 'Imagem',
+        ];
+    }
+
+    public function saveProfile()
+    {
+        $modelProfile = new Profile();
+
+        $modelProfile->attributes = $this->attributes;
+
+        if ($modelProfile->validate()) {
+            return false;
+        }else{
+            var_dump($modelProfile->errors);
+            echo '<hr>';
+            var_dump($modelProfile->attributes);
+        }
+
+        die;
+
+        $modelProfile->save();
+
+        $this->uploadImage($modelProfile->id);
+
+        return true;
+    }
+
+    private function uploadImage($idCreatedProfile)
+    {
+        $modelUpload = new UploadForm();
+        $this->image_file = UploadedFile::getInstance($this, 'image_file');
+        $modelUpload->image_file = $this->image_file;
+        $modelUpload->upload($idCreatedProfile);
+
+        $nameUploadedImage = "{$modelUpload->image_file->baseName}_0{$idCreatedProfile}";
+
+        $this->saveImage($nameUploadedImage, $idCreatedProfile);
+    }
+
+    private function saveImage($nameUploadedImage, $idCreatedProfile)
+    {
+        $modelImage = new Image();
+
+        $modelImage->extension = $this->image_file->extension;
+        $modelImage->name = $nameUploadedImage;
+        $modelImage->post_id = $idCreatedProfile;
+
+        $modelImage->save();
+    }
+}
