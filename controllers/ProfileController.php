cc7c80eccadd6b92057f166cfbcb43effd5e0ee9
jonas-amilton <jonas96@edu.unisinos.br>
2025-02-10 22:56:18 -0300

    feat: criado consulta para exibir publicações e dados do usuário logado no seu perfil

--- controllers/ProfileController.php
+++ controllers/ProfileController.php
@@ -3,6 +3,7 @@
 namespace app\controllers;
 
 use app\models\ProfileForm;
+use app\models\User;
 use yii\web\Controller;
 use Yii;
 
@@ -12,7 +13,33 @@ class ProfileController extends Controller
     {
         $modelProfileForm = new ProfileForm();
 
-        return $this->render('index', compact('modelProfileForm'));
+        $user = User::findOne(['id' => Yii::$app->user->identity->id]);
+        $profile = null;
+        $posts = [];
+
+        if ($user) {
+            $posts = $user->posts;
+
+            if ($user->profiles) {
+                $profile = array_reverse($user->profiles)[0];
+
+                $profilePhoto = "{$profile->avatars[0]->name}.{$profile->avatars[0]->extension}";
+            }
+        }
+
+        if (!$profile) {
+            $profilePhoto = 'avatar-default.png';
+        }
+
+        return $this->render(
+            'index',
+            compact(
+                'modelProfileForm',
+                'profile',
+                'profilePhoto',
+                'posts'
+            )
+        );
     }
 
     public function actionEdit()
--- views/profile/index.php
+++ views/profile/index.php
@@ -3,7 +3,7 @@
         <img src="https://images.pexels.com/photos/268941/pexels-photo-268941.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
             class="card-img-top profile-cover"
             alt="...">
-        <img src="https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        <img src="<?= Yii::getAlias('@showImage/') . "{$profilePhoto}"; ?>"
             class="card-img-top profile-img"
             alt="...">
     </div>
@@ -15,80 +15,40 @@
             <blockquote class="blockquote">
                 <p><?= Yii::$app->user->identity->username; ?>#<?= Yii::$app->user->identity->id; ?></p>
             </blockquote>
-            <figcaption class="blockquote-footer">
-                Someone famous in <cite title="Source Title">Bio</cite>
-            </figcaption>
+            <?php if ($profile): ?>
+                <figcaption class="blockquote-footer">
+                    <?= "{$profile->age} anos | {$profile->bio}"; ?> <cite title="Source Title">Bio</cite>
+                </figcaption>
+            <?php endif; ?>
         </figure>
     </div>
 </div>
 
-<div class="card mb-3" style="width: 100%;">
-    <div class="row g-0">
-        <div class="col-md-4">
-            <img src="https://images.pexels.com/photos/28184434/pexels-photo-28184434/free-photo-of-mar-natureza-agua-azul.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-                class="img-fluid rounded-start"
-                alt="...">
-        </div>
-        <div class="col-md-8">
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+<?php foreach ($posts as $post): ?>
+    <div class="card mb-3" style="width: 100%;">
+        <div class="row g-0">
+            <div class="col-md-4">
+                <?php foreach ($post->images as $image): ?>
+                    <img src="<?= Yii::getAlias('@showImage/') . "{$image->name}.{$image->extension}"; ?>"
+                        class="img-fluid rounded-start"
+                        alt="...">
+                <?php endforeach; ?>
+            </div>
+            <div class="col-md-8">
+                <div class="card-body">
+                    <h5 class="card-title"><?= $post->title; ?></h5>
+                    <p class="card-text"><?= $post->content; ?></p>
+                    <p class="card-text">
+                        <small class="text-body-secondary">
+                            Publicado em: <?= date('d/m/Y h:m', strtotime($post->created_at)); ?>
+                        </small>
+                    </p>
+                </div>
             </div>
         </div>
+        <?= $this->render('../layouts/partials/_btn_delete', ['id' => $post->id, 'userId' => $post->user_id]); ?>
     </div>
-</div>
-
-<div class="card mb-3" style="width: 100%;">
-    <div class="row g-0">
-        <div class="col-md-4">
-            <img src="https://images.pexels.com/photos/29326451/pexels-photo-29326451/free-photo-of-naufragio-rustico-em-uma-praia-intocada.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-                class="img-fluid rounded-start"
-                alt="...">
-        </div>
-        <div class="col-md-8">
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
-            </div>
-        </div>
-    </div>
-</div>
-
-<div class="card mb-3" style="width: 100%;">
-    <div class="row g-0">
-        <div class="col-md-4">
-            <img src="https://images.pexels.com/photos/30221869/pexels-photo-30221869/free-photo-of-penhascos-costeiros-dramaticos-na-madeira-portugal.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-                class="img-fluid rounded-start"
-                alt="...">
-        </div>
-        <div class="col-md-8">
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
-            </div>
-        </div>
-    </div>
-</div>
-
-<div class="card mb-3" style="width: 100%;">
-    <div class="row g-0">
-        <div class="col-md-4">
-            <img src="https://images.pexels.com/photos/30481070/pexels-photo-30481070.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-                class="img-fluid rounded-start"
-                alt="...">
-        </div>
-        <div class="col-md-8">
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
-            </div>
-        </div>
-    </div>
-</div>
+<?php endforeach; ?>
 
 <?= $this->render(
     '../layouts/partials/_edit_profile_modal',
